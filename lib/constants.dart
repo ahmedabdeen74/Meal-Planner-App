def1c051e993e5ea86ef44d0ec1a12bb7d2957aa
Ahmed <ahmedabdeen1408@gmail.com>
2025-07-10 16:36:49 +0300

    add constants file

---
+++ lib/constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Colors.black;
+const kSecondaryColor = Colors.white;
+//const kGreyColor = Color(0xff878787);
+void hideKeyboardFromScreen(BuildContext context) {
+  final currentFocus = FocusScope.of(context);
+  if (!currentFocus.hasPrimaryFocus && currentFocus.focusedChild != null) {
+    FocusManager.instance.primaryFocus?.unfocus();
+  }
+}
+
+OutlineInputBorder buildBorder() {
+  return OutlineInputBorder(
+    borderRadius: BorderRadius.circular(16),
+    borderSide: const BorderSide(color: kSecondaryColor),
+  );
+}
