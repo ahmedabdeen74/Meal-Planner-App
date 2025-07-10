3d68e4801e552ec4699eb83d8eba7c0b461bf084
Ahmed <ahmedabdeen1408@gmail.com>
2025-07-10 14:34:03 +0300

    add fonts and styles

---
+++ lib/core/utility/styles.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  // splash 
+   static const textStyle61 = TextStyle(
+    fontSize: 61,
+    fontWeight: FontWeight.w400,
+     fontFamily : 'Fleur De Leah',
+     color: Color(0xffFFCE52)
+  );
+  static const textStyleMedium14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+     fontFamily : 'Noto Sans'
+  );
+  static const textStyleRegular18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    fontFamily : 'Noto Sans'
+  );
+
+
+  // auth features 
+  static const textStyleMedium13 = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w500,
+    fontFamily : 'Noto Sans'
+  );
+  static const textStyleLight13 = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w300,
+    color: Color(0xff4E4E4E), // #232323
+    fontFamily : 'Noto Sans'
+  );
+  static const textStyleSemibold13 = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w600,
+    color: Colors.white,
+    fontFamily : 'Noto Sans'
+  );
+  static const textStyleLight14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w300,
+   fontFamily : 'Noto Sans'
+  );
+  static const textStyleBold14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w700,
+    fontFamily : 'Noto Sans'
+  );
+  static const textStyleExtralight36 = TextStyle(
+    fontSize: 36,
+    fontWeight: FontWeight.w200,
+   fontFamily : 'Noto Sans'
+  );
+  static const textStyleBold36 = TextStyle(
+    fontSize: 36,
+    fontWeight: FontWeight.bold,
+    fontFamily : 'Noto Sans'
+  );
+
+
+  // home features 
+  static const textStyleregular36 = TextStyle(
+    fontSize: 36,
+    fontWeight: FontWeight.w400,
+    fontFamily : 'Hurricane'
+  );
+  static const textStyleBold24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+    fontFamily : 'Noto Sans'
+  );
+  static const textStyleLight16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w300,
+    fontFamily : 'Noto Sans'
+  );
+  static const textStyleMedium18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    fontFamily : 'Noto Sans'
+  );
+  static const textStyleregular12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    fontFamily : 'Noto Sans'
+  );
+  static const textStyleLight12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w300,
+    fontFamily : 'Noto Sans'
+  );
+  static const textStyleSemibold21 = TextStyle(
+    fontSize: 21,
+    fontWeight: FontWeight.w600,
+    fontFamily : 'Noto Sans'
+  );
+
+
+  // meal details 
+
+  static const textStyleregular15 = TextStyle(
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+    // font family : Noto Serif
+  );
+  static const textStyleSemibold24 = TextStyle(
+    fontSize:24,
+    fontWeight: FontWeight.w600,
+    // font family : Noto Serif
+  );
+
+
+  // calendar view 
+  static const textStyleLight21 = TextStyle(
+    fontSize: 21,
+    fontWeight: FontWeight.w300,
+    fontFamily : 'Noto Sans'
+  );
+  static const textStyleMedium10 = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.w500,
+    fontFamily : 'Noto Sans'
+  );
+}
